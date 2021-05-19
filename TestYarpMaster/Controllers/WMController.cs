5c1fbf03e59a66710752a7493a641636581974eb
Tarek Salah <tareksalah43@hotmail.com>
2021-05-19 16:41:00 +0200

    Add project files.

---
+++ TestYarpMaster/Controllers/WMController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TestYarpMaster.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WMController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Master", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly ILogger<WMController> _logger;
+
+        public WMController(IHttpClientFactory clientFactory, ILogger<WMController> logger)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetDeailt")]
+        public async Task<IEnumerable<WeatherForecast>> GetDeailAsync()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                "https://testyarpdetail.dev/WD/Get");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                await using var responseStream = await response.Content.ReadAsStreamAsync();
+                var result = await JsonSerializer.DeserializeAsync
+                    <IEnumerable<WeatherForecast>>(responseStream);
+                return result;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ testyarppfx.ps1
@@ -0,0 +1,5 @@
+﻿$cert = New-SelfSignedCertificate -DnsName @("testyarpmaster.dev", "testyarpdetail.dev", "host.docker.internal", "localhost") -CertStoreLocation "cert:\LocalMachine\My"
+$rootCertPathPfx = "$env:USERPROFILE/.aspnet/https/testyarpcert.pfx"
+$password = ConvertTo-SecureString '123123' -AsPlainText -Force
+$cert | Export-PfxCertificate -FilePath $rootCertPathPfx -Password $password
+$rootCert = $(Import-PfxCertificate -FilePath $rootCertPathPfx -CertStoreLocation 'Cert:\LocalMachine\Root' -Password $password)
